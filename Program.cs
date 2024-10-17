80656c340d628e078dbe3953b67ecd109ae09e62
Виктор Корнеплод <yarmisikov@gmail.com>
2024-10-17 21:32:27 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1510
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задача 1
+            //Console.WriteLine("a=");
+            //double a = Convert.ToDouble(Console.ReadLine());
+            //if (a > 0)
+            //{
+            //    ++a;
+            //}
+            //else
+            //{
+            //    a -= 2;
+            //}
+            //Console.WriteLine(a);
+            //Задача 2
+            //Console.WriteLine("a=");
+            //double a = Convert.ToDouble(Console.ReadLine());
+            //if (a > 0)
+            //{
+            //    ++a;
+            //}
+            //Console.WriteLine(a);
+            //Задача 3
+            //Console.Write("a=");
+            //double a = Convert.ToDouble(Console.ReadLine());
+            //if (a>0)
+            //{
+            //    ++a;
+            //}
+            //if(a==0)
+            //{
+
+            //    a = 10; 
+            //}
+            //if(a<0)
+            //{
+            //    a-=2;
+            //}
+            //Console.WriteLine(a);
+            //Задача 4
+            //Console.Write("a=");
+            //int a = Convert.ToInt32(Console.ReadLine());
+            //if (a % 2 == 0)
+            //{
+            //    Console.WriteLine("Четное");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Нечетное");
+            //}
+            //Задача 5
+            //Console.Write("a=");
+            //int a = Convert.ToInt32(Console.ReadLine());
+            //if (a > 0)
+            //{
+            //    Console.WriteLine("Полож");
+            //}
+            //if (a < 0)
+            //{
+            //    Console.WriteLine("Отриц");
+            //}
+            //if (a == 0)
+            //{
+            //    Console.WriteLine("Нул");
+            //}
+            //Задача 6
+            //Console.Write("a=");
+            //int a = Convert.ToInt32(Console.ReadLine());
+            //if (a > 20000 && a < 30000)
+            //{
+            //    Console.WriteLine("Входит");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Невх");
+            //}
+            //Задача 7
+            //Console.Write("Введите год=");
+            //int a = Convert.ToInt32(Console.ReadLine());
+            //if (a % 4 == 0)
+            //{
+            //    Console.WriteLine("Високосный");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Нет");
+            //}
+            //int x = 3;
+            //int y = 2;
+
+            //int z = x < y ? (x + y) : (x - y);
+            //Console.WriteLine(z);
+            //Console.Write("x=");
+            //int x = Convert.ToInt32(Console.ReadLine());
+            //int y = 0;
+            //if (x>0)
+            //{
+            //    y = x + 1;
+            //}
+            //else
+            //{
+            //    y = x - 1;
+            //}
+            //int y = x > 0? x+1 : x-1;
+            //Console.WriteLine(y);
+            Console.ReadLine();
+        }
+    }
+}
